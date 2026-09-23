d81b4c31bd22bc2a8c69bfef7e390303de6f61a3
unknown <wulandazen0@gmail.com>
2026-09-23 14:19:01 +0700

    Pertemuan 3 - Routing, Controller, dan Blade dasar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,4 +16,9 @@ class HomeController extends Controller
     {
         return view('tentang');
     }
+
+    public function kontak()
+    {
+        return view('kontak');
+    }
 }
---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'kontak')
+
+@section('content')
+    <h1>Kontak</h1>
+    <p>Untuk informasi lebih lanjut, silakan hubungi kami melalui email:
+@endsection
